b5bb47d7572c86a77498a3532869b54a466e93ec
shuva9800 <ffshuva@gmail.com>
2024-02-09 00:19:21 +0530

    ongoing

---
+++ src/components/Footer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <div className='footer-top'>
+            <div>
+                <img src='https://s3-alpha-sig.figma.com/img/2544/a167/04e564242973d6e3db745e96ce43e6c2?Expires=1708300800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=axz24vjTRngqjTPzt2xD8wNkfac8PnYtx-BTGbxtVdiiPOw6aJHj~3xwXZqmwH0wW5X2p6XjBOwQku47wwlgjADUYuVnnOf7wSnmeHSSKpsMiTWweLLXXekAC-f3JbuKGXsNXOYdPU4p6CLcPrntT3nlxEaRXaOzUaIUuEeqljhhW0NkG-~RiaexEV4S28HfyG1VUvVFOum-kTQzX5oci0SfyWn61E0V3SOAjU61B-TgE2D9OtrMEsdQa96TKOU1fFDL2BerO85cwg9AmI-RIwo5pTE6HalxjrET7mu5PouAVovwXVpnmkD-li~FIVUuGEKL59qyUZx7sgsUTq1WKw__'
+                />
+            </div>
+            <div>
+                <img src='https://s3-alpha-sig.figma.com/img/7f85/33db/7c7cd6a22163199a44a7b255460c59fc?Expires=1708300800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=F~aBCEuRO7uCC6MAXml7pZvQQFiFmFSQJTTLpr56TQfh-Dnn~gUY04duj930zAua0ztGr8bI2o7cOBfaASE4K6Ow-2eAD7rLveEVq-l-k2sOUUNN3oufkJHEWc5QSpQ5EE4jiVlORydNq24Qy1odjjK6ndbi7-pRr2pq5GW5dvs-JacTUSd5Pz9RpYfZ8sErA8WCWyU-sxuj80tYK9Dr1nXnJmtpOJ77itmdvrIYbQg352V055OY7s4hHxbhY2S0pGliVKjQ~AcNIjNLvqUnqNUpuDmlTTLgu3W0HQqYK0YWtWuH5OOJVn85R0GGIRdqmROKpu0Do2nUtiEPGSC-Gg_'
+
+                />
+                <img src='https://s3-alpha-sig.figma.com/img/2240/880b/4047ad0e4277b91a714440107fca8ac4?Expires=1708300800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Xl8f5p4Y4hcfa6n6wkJNOobAZRf9DEakocQKzSk2LG6Y4MPKmeOneM1zrmk2z30Vuden9PKsoUXxZ0TWymU~ZiTVsclvsJsIwA2a7fqAV7i5-AlRqy4WjhuM-lwrl~9MFpBqnDP1oSFqITYF6eMNrkg9VO8-xVc~N~4YYhThPY2vXV~8KIs5bn6ZjwmmKhtHmgmgKuk-LUMd83egIKrj8L-Ap5T2EA9wO9J95XQO2WITqbea8IAPMdr33DU6FT44RvtLtTcAk6KGsyIMqjJcXW4DFqLSA4UAV7EFtx66oCw4hnYxjvGL~DmXkYD2~63I6HEICnVKlv4qGajbM5jlDw__'
+                />
+            </div>
+        </div>
+        <div className='footer-mid'>
+            <ul className='footer-list'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Check Our Designs</li>
+                <li>Contact us</li>
+
+            </ul>
+        </div>
+        <div>
+            <p>copyright ©2023 Stylesterai</p>
+        </div>
+    </div>
+  )
+}
